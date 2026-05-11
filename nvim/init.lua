122530bf53e2df58c2de279ce70dadeb31e2991f
cotton <chinoknct@gmail.com>
2026-05-11 18:58:21 +0900

    feat(nvim): add keymap to copy current file path
    Map <Leader>yp to copy the buffer's relative path to the system
    clipboard for quick sharing in chats and reviews.

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,11 @@ require("config.lazy")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+local function copy_current_file_path()
+  local path = vim.fn.expand "%:."
+  vim.fn.setreg("*", path)
+  vim.api.nvim_echo({ { "Copied: " .. path, "None" } }, true, {})
+end
+vim.keymap.set("n", "<Leader>yp", copy_current_file_path, { noremap = true, silent = true })
+
